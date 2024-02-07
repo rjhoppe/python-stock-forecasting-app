c69edc8dd928fd187bcaff41b3c38b748cdc02a8
rjhoppe-W <48058894+rjhoppe@users.noreply.github.com>
2024-02-07 10:05:38 -0500

    feat! Cleaned up init build

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import streamlit as st
 from datetime import date
 
 import yfinance as yf
-import fbprophet import Prophet
-from fbprophet.plot import plot_plotly
+from prophet import Prophet
+from prophet.plot import plot_plotly
 from plotly import graph_objs as go
 
 START = "2020-01-01"
@@ -18,7 +18,7 @@ selected_stock = st.selectbox("Select dataset for prediction",
 n_years = st.slider("Years of prediction:", 1 , 4)
 period = n_years * 365
 
-@st.cache
+@st.cache_data
 def load_data(ticker):
   data = yf.download(ticker, START, TODAY)
   # Put data in first column
@@ -34,29 +34,31 @@ st.write(data.tail())
 
 def plot_raw_data():
   fig = go.Figure()
-  fig.add_trace(go.Scatter(x=data['Data'], y=data['Open'], name='stock_open'))
-    fig.add_trace(go.Scatter(x=data['Data'], y=data['Close'], name='stock_close'))
-    fig.layout.update(title_text="Time Series Data", xaxis_rangeslider_visible=True)
-    st.plotly_chart(fig)
+  fig.add_trace(go.Scatter(x=data['Date'], y=data['Open'], name='stock_open'))
+  fig.add_trace(go.Scatter(x=data['Date'], y=data['Close'], name='stock_close'))
+  fig.layout.update(title_text="Time Series Data", xaxis_rangeslider_visible=True)
+  st.plotly_chart(fig)
 
 plot_raw_data()
 
 # Forecasting
-df_train = data[['Data', 'Close']]
+df_train = data[['Date', 'Close']]
 # This is how Prophet requires the data
-df_train = df_train.rename(columns={"Date": "ds", "Closing": "y"})
+df_train = df_train.rename(columns={"Date": "ds", "Close": "y"})
+print(df_train)
 
 m = Prophet()
 m.fit(df_train)
 future = m.make_future_dataframe(periods=period)
 forecast = m.predict(future)
 
-st.subheader("Forecast data")
+st.subheader("Forecast Data")
 st.write(forecast.tail())
 
+st.write('Forecast Data')
 fig1 = plot_plotly(m, forecast)
 st.plotly_chart(fig1)
 
-st.write('forecast_components')
+st.write('forecast components')
 fig2 = m.plot_components(forecast)
 st.write(fig2)
